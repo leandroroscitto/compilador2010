e78ab6ce3563055350226f0a09968c3a8cdf45c0
leandroroscitto <leandroroscitto@de0168e8-1b52-9d30-e01d-d39c8bc108fe>
2010-09-30 17:57:13 +0000

    Correccion de mensajes de error. Se capturan las excepciones sintacticas y lexicas en Principal2. Correccion de lambda no utilizado en sentencia de procedimiento'.

--- src/principal/Principal2.java
+++ src/principal/Principal2.java
@@ -9,40 +9,45 @@ import excepciones.ExcepASintatico;
 
 public class Principal2 {
 
-    public static void main(String[] args) throws ExcepALexico, ExcepASintatico {
-        if (args.length > 0) {
-            PrintStream Salida;
-            try {
-                if (args.length == 1) {
-                    // Hay un solo argumento, lo considero el archivo de entrada
-                    // La salida es por pantalla
-                    Salida = System.out;
-                } else {
-                    // Hay al menos 2 argumentos, el primero es el archivo de entrada
-                    // y el segundo el de salida
-                    Salida = new PrintStream(args[1]);
-                }
-                Salida.println("Archivo de entrada:'" + args[0] + "':");
-
-                // Crea el analizador sintactico, con el nombre
-                // del archivo de entrada como parametro
-                Parser ASintactico = new Parser(args[0]);
-
-                // Comienza con el no terminal programa, seguido
-                // por el final de archivo
-                ASintactico.programa();
-                ASintactico.eof();
-
-                // Si llega aqui sin producer una excepcion, el codigo
-                // de entrada es sintacticamente correcto
-
-                Salida.println("El proceso se completo con exito.");
-            } catch (IOException e) {
-                System.out.println("IOException capturada, no se pudo leer del archivo.");
-            }
+  public static void main(String[] args) throws ExcepALexico, ExcepASintatico {
+    if (args.length > 0) {
+      PrintStream Salida;
+      try {
+        if (args.length == 1) {
+          // Hay un solo argumento, lo considero el archivo de entrada
+          // La salida es por pantalla
+          Salida = System.out;
         } else {
-            // Falto el parametro de entrada
-            System.out.println("De como parametro al menos un nombre para el archivo de entrada.");
+          // Hay al menos 2 argumentos, el primero es el archivo de entrada
+          // y el segundo el de salida
+          Salida = new PrintStream(args[1]);
         }
+        Salida.println("Archivo de entrada:'" + args[0] + "':");
+
+        // Crea el analizador sintactico, con el nombre
+        // del archivo de entrada como parametro
+        Parser ASintactico = new Parser(args[0]);
+
+        // Comienza con el no terminal programa, seguido
+        // por el final de archivo
+        ASintactico.programa();
+        ASintactico.eof();
+
+        // Si llega aqui sin producer una excepcion, el codigo
+        // de entrada es sintacticamente correcto
+
+        Salida.println("El proceso se completo con exito.");
+      } catch (IOException e) {
+        System.out.println("IOException capturada, no se pudo leer del archivo.");
+      } catch (ExcepALexico e) {
+        System.out.println("Error lexico: " + e.getMessage());
+      } catch (ExcepASintatico e) {
+        System.out.println("Error sintactico: " + e.getMessage());
+      }
+    } else {
+      // Falto el parametro de entrada
+      System.out.println("De como parametro al menos un nombre para el archivo de entrada.");
     }
+    System.out.println();
+  }
 }
